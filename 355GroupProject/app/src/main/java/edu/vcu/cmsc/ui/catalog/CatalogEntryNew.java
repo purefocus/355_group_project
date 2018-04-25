854af4f7e110de8c7487fb74aab1d0c6f148e1d8
reedcn <reed.christinen@gmail.com>
2018-04-25 19:29:29 -0400

    upload image from device for catalog entry, put into ArrayList<ImageView>

--- 355GroupProject/app/src/main/java/edu/vcu/cmsc/ui/catalog/CatalogEntryNew.java
+++ 355GroupProject/app/src/main/java/edu/vcu/cmsc/ui/catalog/CatalogEntryNew.java
@@ -3,12 +3,22 @@ package edu.vcu.cmsc.ui.catalog;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.vcu.cmsc.R;
 import edu.vcu.cmsc.data.CatalogEntryData;
@@ -20,13 +30,19 @@ import edu.vcu.cmsc.data.CatalogEntryData;
 // TODO: Link from main notification page
 
 public class CatalogEntryNew extends Activity {
+    private ImageView imageView;
+
+    private Uri filePath;
+    ArrayList<ImageView> images;
+    private final int PICK_IMAGE_REQUEST =1;
     int checkBool = 0;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog_new);
-    }
+        }
 
     public void check_for_sale(View view) {
         boolean checked = ((CheckBox) view).isChecked();
@@ -38,15 +54,47 @@ public class CatalogEntryNew extends Activity {
         }
     }
 
+    private void selectImage(){
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(i, "Select Picture from Gallery"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+  if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+          && data != null && data.getData() != null )
+  {
+    filePath = data.getData();
+    try {
+       ImageView catImg = findViewById(R.id.cat_img);
+      Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+      catImg.setImageBitmap(bitmap);
+        images = new ArrayList<ImageView>();
+        images.add((ImageView) findViewById(R.id.cat_img));
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
+
     public void btn_make_entry(View v) {
         int correctInput = 1;
         Context context = getApplicationContext();
         CharSequence message = "Successfully made a new catalog entry.";
         int duration = Toast.LENGTH_SHORT;
 
-        //if (v.getId() == R.id.btn_upload_pic) {
-        //}
+        if (v.getId() == R.id.btn_upload_pic) {
+            selectImage();
+        }
 
+        if (v.getId() == R.id.btn_upload_from_url) {
+            //
+        }
 
         if (v.getId() == R.id.btn_make_catalog_entry) {
             EditText titleField = (EditText)findViewById(R.id.field_artifact_title);
