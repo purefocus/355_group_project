e22e6640ca24657b262775b83433ddea6162d4cc
reedcn <reed.christinen@gmail.com>
2018-04-25 23:07:05 -0400

    edit SettingsActivity to add "Change Password", added activity_settings_change_pass.xml and ChangePass class

--- 355GroupProject/app/src/main/java/edu/vcu/cmsc/ui/catalog/CatalogEntryNew.java
+++ 355GroupProject/app/src/main/java/edu/vcu/cmsc/ui/catalog/CatalogEntryNew.java
@@ -1,13 +1,17 @@
 package edu.vcu.cmsc.ui.catalog;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -30,6 +34,7 @@ import edu.vcu.cmsc.data.CatalogEntryData;
 // TODO: Link from main notification page
 
 public class CatalogEntryNew extends Activity {
+    private Button btnChoose, btnUpload;
     private ImageView imageView;
 
     private Uri filePath;
@@ -63,24 +68,21 @@ public class CatalogEntryNew extends Activity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-  if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-          && data != null && data.getData() != null )
-  {
-    filePath = data.getData();
-    try {
-       ImageView catImg = findViewById(R.id.cat_img);
-      Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-      catImg.setImageBitmap(bitmap);
-        images = new ArrayList<ImageView>();
-        images.add((ImageView) findViewById(R.id.cat_img));
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null ) {
+            filePath = data.getData();
+            try {
+                ImageView catImg = findViewById(R.id.cat_img);
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                catImg.setImageBitmap(bitmap);
+                images = new ArrayList<ImageView>();
+                images.add((ImageView) findViewById(R.id.cat_img));
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            }
     }
-    catch (IOException e)
-    {
-      e.printStackTrace();
-    }
-  }
-}
 
     public void btn_make_entry(View v) {
         int correctInput = 1;
