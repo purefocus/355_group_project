aaa3a68d47dedf6c543b6bb18de6ac9f257cbd10
Kevin Ly Ngo <ngokl@mymail.vcu.edu>
2018-05-01 00:42:23 -0400

    Added the chat board

--- 355GroupProject/app/src/main/java/edu/vcu/cmsc/data/ChatThreadData.java
+++ 355GroupProject/app/src/main/java/edu/vcu/cmsc/data/ChatThreadData.java
@@ -4,6 +4,14 @@ package edu.vcu.cmsc.data;
 public class ChatThreadData
 {
 
+    public String threadKey;
 
+    public String author;
+    public long time;
+    public String message;
 
+    public ChatThreadData()
+    {
+
+    }
 }
--- 355GroupProject/app/src/main/java/edu/vcu/cmsc/ui/chat/ThreadPostActivity.java
+++ 355GroupProject/app/src/main/java/edu/vcu/cmsc/ui/chat/ThreadPostActivity.java
@@ -1,17 +1,38 @@
 package edu.vcu.cmsc.ui.chat;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Date;
 
 import edu.vcu.cmsc.R;
 
-public class ThreadPostActivity extends AppCompatActivity
+public class ThreadPostActivity extends FragmentActivity
 {
-	
+
+	private ThreadFragment mthread;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+
+
 		setContentView(R.layout.activity_thread_post);
+
+		String author = getIntent().getStringExtra("author");
+
+		long posttime = getIntent().getLongExtra("time",0);
+
+		String message = getIntent().getStringExtra("message");
+		((TextView)findViewById(R.id.thread_post_author)).setText(author);
+		((TextView)findViewById(R.id.thread_post_time)).setText(new Date(posttime).toString());
+		((TextView)findViewById(R.id.thread_post_message)).setText(message);
+
+
 	}
 }
